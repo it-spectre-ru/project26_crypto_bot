12ec3c237cdf8e903f908d5bc86f942c6c8e0eb5
s <s>
2022-07-21 18:11:23 +0500

    get_ticker info

--- main.py
+++ main.py
@@ -9,8 +9,19 @@ def get_info():
   return response.text
 
 
+def get_ticker():
+  response = requests.get(url='https://yobit.net/api/3/ticker/eth_btc-xrp_btc')
+
+  with open('ticker.txt', 'w') as file:
+    file.write(response.text)
+
+  return response.text
+
+
+
 def main():
-  print(get_info())
+  # print(get_info())
+  print(get_ticker())
 
 
 if __name__ == '__main__':
