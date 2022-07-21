6da66b89d49df93d38b33136e9be710d241f7e30
s <s>
2022-07-21 18:56:38 +0500

    get buy sell amount

--- main.py
+++ main.py
@@ -39,13 +39,35 @@ def get_depth(coin1='btc',coin2='usd', limit=150):
   return f"Total bids: {total_bids_amount} $"
 
 
+def get_trades(coin1='btc',coin2='usd', limit=150):
+  response = requests.get(url=f'https://yobit.net/api/3/trades/{coin1}_{coin2}?limit={limit}&ignore_invalid=1')
+
+  with open('trades.txt', 'w') as file:
+    file.write(response.text)
+
+  total_trade_ask = 0
+  total_trade_bid = 0
+
+  for item in response.json()[f'{coin1}_{coin2}']:
+    if item['type'] == 'ask':
+      total_trade_ask += item['price'] * item['amount']
+    else:
+      total_trade_bid += item['price'] * item['amount']
+
+  info = f'[-] TOTAL {coin1} SELL: {round(total_trade_ask, 2)} $\n[+] TOTAL {coin1} BUY: {round(total_trade_bid, 2)} $'
+
+  return info
+
+
+
 
 def main():
   # print(get_info())
   # print(get_ticker())
   # print(get_ticker(coin1='eth'))
-  print(get_depth())
-  print(get_depth(coin1='eth', limit=2000))
+  # print(get_depth())
+  # print(get_depth(coin1='eth', limit=2000))
+  print(get_trades())
 
 
 if __name__ == '__main__':
