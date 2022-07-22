e3284e3d602edd654c09516ec8e2997ed6ae29f0
s <s>
2022-07-22 13:56:44 +0500

    get coin adress

--- main2.py
+++ main2.py
@@ -24,10 +24,33 @@ def get_info():
   return response.json()
 
 
+
+def get_deposit_address(coin_name="btc"):
+  values = dict()
+  values["method"] = "GetDepositAddress"
+  values["coinName"] = coin_name
+  values["need_new"] = 0
+  values["nonce"] = str(int(time.time()))
+
+  body = urlencode(values).encode("utf-8")
+  sign = hmac.new(API_SECRET.encode("utf-8"), body, hashlib.sha512).hexdigest()
+
+  headers = {
+    "key": API_KEY,
+    "sign": sign
+  }
+
+  response = requests.post(url="https://yobit.net/tapi/", headers=headers, data=values)
+  return response.json()
+
+
   
 
 def main():
-  print(get_info())
+  # print(get_info())
+
+  coin_name = input("Enter coin name: ")
+  print(get_deposit_address(coin_name=coin_name))
 
 
 if __name__ == "__main__":
